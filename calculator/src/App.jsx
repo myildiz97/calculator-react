e4a925bb86be29d3fd33bdd478f01d687a309715
Mehmet Yıldız <myildizwork@gmail.com>
2023-07-30 13:19:17 +0300

    problems fixed

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -5,6 +5,10 @@ const App = () => {
   const [calculation, setCalculation] = useState("");
   const [result, setResult] = useState(null);
   const [flag, setFlag] = useState(false);
+  const [limitCurr, setLimitCurr] = useState(null);
+  const [limitCalc, setLimitCalc] = useState(null);
+  const [limitFlag, setLimitFlag] = useState(false);
+
 
   const operators = ["+", "-", "*", "/"];
   const opsWoSub = ["+", "*", "/"];
@@ -12,6 +16,26 @@ const App = () => {
   const handleClick = (e) => {
     let value = e.target.textContent;
     if (value === "x") value = "*";
+    if (current.length > 15) {
+      if (!operators.includes(value)) {
+        setLimitCurr("DIGIT LIMIT MET");
+        setTimeout(() => {
+          setLimitCurr(null);
+        }, 1500);
+        return;
+      };
+    };
+
+    if (calculation.length > 45) {
+      if (value !== "=" && !limitFlag) {
+        setLimitCalc("DIGIT LIMIT MET");
+        setTimeout(() => {
+          setLimitCalc(null);
+        }, 1500);
+        return;
+      };
+    };
+    
     let lastResult = result;
     if (lastResult) {
       setResult(null);
@@ -25,8 +49,8 @@ const App = () => {
             !current.includes(value) && setCurrent(prev => prev + value);
           } else {
             setCurrent(prev => prev + value);
-          }
-        }
+          };
+        };
       } else if (operators.includes(value)) {
         setCurrent(value);
       };
@@ -35,7 +59,7 @@ const App = () => {
       if (value === ".") {
         !current.includes(".") && setCalculation(prev => prev + value);
       } else if (opsWoSub.includes(value)) {
-        !operators.includes(calculation.at(-1)) && setCalculation(prev => prev + value);
+        !opsWoSub.includes(calculation.at(-1)) ? setCalculation(prev => prev + value) : setCalculation(prev => prev.slice(0, -1) + value);
       } else if (value === "-") {
         (calculation.at(-1) !== "-" || (calculation.at(-1) === "-" && calculation.at(-2) !== "-")) && setCalculation(prev => prev + value);
       } else {
@@ -46,9 +70,9 @@ const App = () => {
             setCalculation(value);
           } else {
             calculation !== "0" &&  setCalculation(prev => prev + value);
-          }
+          };
         };
-      }
+      };
     } else {
       operators.includes(value) ? setCalculation(lastResult + value) : setCalculation(value);
       setFlag(false);
@@ -65,20 +89,29 @@ const App = () => {
   const handleEqual = (e) => {
     if (!flag ) {
       const value = e.target.textContent;
-      let calc = operators.includes(calculation.at(-1)) ? calculation.slice(0, -1) : calculation;
-      console.log(calc);
+      let calc = operators.includes(calculation.at(-1)) ? operators.includes(calculation.at(-2)) ? 
+        calculation.slice(0, -2) : calculation.slice(0, -1) : calculation;
+      calc = calc.includes("--") ? calc.replace(/--/g, "+") : calc;
+      const multiOp = calc.match(/[+\-*\/]+/g);
+      multiOp.forEach(op => {
+        if (op.length > 2 || (op.length === 2 && op.at(0) === "-" && op.at(-1) !== "-")) calc = calc.replace(op, op.at(-1));
+        if (operators.includes(calc.at(-1))) calc = calc.slice(0, -1);
+      });
+
       const result = eval(calc);
       setResult(result);
-      operators.includes(calculation.at(-1)) ? setCalculation(prev => prev.slice(0, -1) + value + result) : setCalculation(prev => prev + value + result);
+      operators.includes(calculation.at(-1)) ? operators.includes(calculation.at(-2)) ? 
+        setCalculation(prev => prev.slice(0, -2) + value + result) : setCalculation(prev => prev.slice(0, -1) + value + result) : setCalculation(prev => prev + value + result);
       setFlag(true);
+      setLimitFlag(true);
     };
   };
 
   return (
     <div className="container">
       <div className="results">
-        <div className="res">{calculation}</div>
-        <div className="curr" id="display">{result || current}</div>
+        <div className="res">{limitCalc ||calculation}</div>
+        <div className="curr" id="display">{limitCurr || result || current}</div>
       </div>
       <div className="btns">
         <div onClick={handleClear} className="btn" id="clear">AC</div>
